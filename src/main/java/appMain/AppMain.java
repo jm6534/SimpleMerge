8e7b073a82ac382089ca1a52b47c51b39ce2dd6e
minn951120 <minn6534@gmail.com>
2018-05-02 14:22:02 +0900

    init project

---
+++ src/main/java/appMain/AppMain.java
@@ -0,0 +1,54 @@
+package appMain;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class AppMain extends Application {
+
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+    }
+
+    /**
+     * 상위 레이아웃을 초기화한다.
+     */
+    public void initRootLayout() {
+        try {
+            // fxml 파일에서 상위 레이아웃을 가져온다.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/resource/view/AppMain.fxml"));
+            rootLayout = (AnchorPane) loader.load();
+
+            // 상위 레이아웃을 포함하는 scene을 보여준다.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 메인 스테이지를 반환한다.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
