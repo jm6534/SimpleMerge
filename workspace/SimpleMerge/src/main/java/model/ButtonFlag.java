3a86b9fbe6976601c23c953ff69303496f83a1df
star7357 <38802212+star7357@users.noreply.github.com>
2018-05-29 01:02:09 +0900

    Model Modification_Line/ButtonFlag/MainModel Class
    Line class : LineColor added. Represent the color for each line. Toggle function added.
    ButtonFlag : saveFlag, editFlag modified to isSaved and isEdited. Also toggle function for each flag added.
    MainModel : CF modified to isCompared. Toggle function added.
    
    Enum class for color might be updated soon.

--- workspace/SimpleMerge/src/main/java/model/ButtonFlag.java
+++ workspace/SimpleMerge/src/main/java/model/ButtonFlag.java
@@ -3,27 +3,33 @@ package model;
 /* Contain info about save, load, edit buttons if the button is clicked
 */
 public class ButtonFlag {
-	private boolean saveFlag;	// Save flag
-	private boolean editFlag;	// Edit flag
+	private boolean isSaved;	// Save flag
+	private boolean isEdited;	// Edit flag
 	
 	public ButtonFlag() {
-		this.saveFlag = false;
-		this.editFlag = false;
+		this.isSaved = false;
+		this.isEdited = false;
 	}
 	
-	public boolean getSaveFlag() {
-		boolean ret = new Boolean(saveFlag);
-		return ret;
+	public boolean isSaved() {
+		return this.isSaved;
 	}	
-	public void setSaveFlag(boolean SF) {
-		this.saveFlag = SF;
+	public void setIsSaved(boolean isSaved) {
+		this.isSaved = isSaved;
 	}
 
-	public boolean getEditFlag() {
-		boolean ret = new Boolean(editFlag);
-		return ret;
+	public boolean isEdited() {
+		return this.isEdited;
 	}	
-	public void setEditFlag(boolean EF) {
-		this.editFlag = EF;
+	public void setIsEdited(boolean isEdited) {
+		this.isEdited = isEdited;
+	}
+	
+	public void toggleIsEdited() {
+		this.isEdited = !isEdited;
+	}
+	
+	public void toggleIsSaved() {
+		this.isSaved = !isSaved;
 	}
 }
